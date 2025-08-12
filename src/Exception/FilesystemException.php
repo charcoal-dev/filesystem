95a3cef087b335bf25330700fa4a02939beaad5f
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2025-08-12 09:43:46 +0500

    refactor: simplify file header and cleanup `FilesystemException` implementation

--- src/Exception/FilesystemException.php
+++ src/Exception/FilesystemException.php
@@ -1,18 +1,14 @@
 <?php
-/*
- * This file is a part of "charcoal-dev/filesystem" package.
- * https://github.com/charcoal-dev/filesystem
- *
- * Copyright (c) Furqan A. Siddiqui <hello@furqansiddiqui.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code or visit following link:
- * https://github.com/charcoal-dev/filesystem/blob/master/LICENSE
+/**
+ * Part of the "charcoal-dev/filesystem" package.
+ * @link https://github.com/charcoal-dev/filesystem
  */
 
 declare(strict_types=1);
 
 namespace Charcoal\Filesystem\Exception;
+
+use Charcoal\Filesystem\Enums\FilesystemError;
 
 /**
  * Class FilesystemException
@@ -20,12 +16,6 @@ namespace Charcoal\Filesystem\Exception;
  */
 class FilesystemException extends \Exception
 {
-    /**
-     * @param \Charcoal\Filesystem\Exception\FilesystemError $error
-     * @param string $message
-     * @param array $data
-     * @param \Throwable|null $previous
-     */
     public function __construct(
         public readonly FilesystemError $error,
         string                          $message = "",
