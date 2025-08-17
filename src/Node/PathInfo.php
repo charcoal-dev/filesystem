40ba313a4af87236ce2b052b48002148f6794168
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2025-08-17 09:25:23 +0500

    add: improve `PathInfo` with parent path resolution, enhanced permissions checks, and error handling via `ErrorHelper`

--- src/Node/PathInfo.php
+++ src/Node/PathInfo.php
@@ -8,6 +8,7 @@ declare(strict_types=1);
 
 namespace Charcoal\Filesystem\Node;
 
+use Charcoal\Base\Support\ErrorHelper;
 use Charcoal\Filesystem\Enums\PathContext;
 use Charcoal\Filesystem\Enums\PathType;
 use Charcoal\Filesystem\Exceptions\InvalidPathException;
@@ -29,6 +30,7 @@ final readonly class PathInfo
     public bool $readable;
     public bool $writable;
     public bool $executable;
+    public string $parent;
 
     /**
      * @param SafePath|string $path
@@ -52,6 +54,7 @@ final readonly class PathInfo
 
         $this->absolute = $absolute;
         $this->basename = basename($this->absolute);
+        $this->parent = dirname($this->absolute);
         $absolute = realpath($absolute);
         if (!$absolute) {
             $this->validated = $path instanceof SafePath;
@@ -62,17 +65,27 @@ final readonly class PathInfo
             return;
         }
 
+        error_clear_last();
         $this->validated = true;
         $this->type = match (true) {
-            is_dir($this->absolute) => PathType::Directory,
-            is_file($this->absolute) => PathType::File,
-            is_link($this->absolute) => PathType::Link,
+            @is_file($this->absolute) => PathType::File,
+            @is_dir($this->absolute) => PathType::Directory,
             default => PathType::Other
         };
 
-        $this->readable = is_readable($this->absolute);
-        $this->writable = is_writable($this->absolute);
-        $this->executable = is_executable($this->absolute);
+        $permissions = $this->type === PathType::File ||
+            $this->type === PathType::Directory;
+
+        $this->readable = $permissions && @is_readable($this->absolute);
+        $this->writable = $permissions && @is_writable($this->absolute);
+        $this->executable = $permissions && @is_executable($this->absolute);
+
+        $error = ErrorHelper::lastErrorToRuntimeException();
+        if ($error) {
+            throw new InvalidPathException($this->absolute,
+                "Cannot instantiate PathInfo; Caught a filesystem error",
+                previous: $error);
+        }
     }
 
     /**
